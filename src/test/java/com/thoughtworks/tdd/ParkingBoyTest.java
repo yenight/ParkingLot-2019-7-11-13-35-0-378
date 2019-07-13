a6b800515ba0b06b2c128d11b86e741b2e66827e
yeweiting <1227061087@qq.com>
2019-07-13 23:34:04 +0800

    [feat: implement story1 ac2]

--- src/test/java/com/thoughtworks/tdd/ParkingBoyTest.java
+++ src/test/java/com/thoughtworks/tdd/ParkingBoyTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 public class ParkingBoyTest {
 
     @Test
-    public void should_return_car_when_park_car_to_parking_log_then_get_it_back() {
+    public void should_return_car_when_park_car_to_parking_lot_then_get_it_back() {
         //given
         Car car = new Car();
         ParkingLot parkingLot = new ParkingLot(new HashMap<>());
@@ -20,5 +20,25 @@ public class ParkingBoyTest {
         Car fetchCar = parkingBoy.fetch(ticket);
         //then
         assertSame(car, fetchCar);
+    }
+
+    @Test
+    public void should_return_cars_when_park_cars_to_parking_lot_then_get_them_back() {
+        //given
+        Car firstCar = new Car();
+        Car secondCar = new Car();
+
+        ParkingLot parkingLot = new ParkingLot(new HashMap<>());
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+
+        //when
+        Ticket firstTicket = parkingBoy.park(firstCar);
+        Car fetchFirstCar = parkingBoy.fetch(firstTicket);
+
+        Ticket secondTicket = parkingBoy.park(secondCar);
+        Car fetchSecondCar = parkingBoy.fetch(secondTicket);
+        //then
+        assertSame(firstCar, fetchFirstCar);
+        assertSame(secondCar, fetchSecondCar);
     }
 }
